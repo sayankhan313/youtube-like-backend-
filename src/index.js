f1c43a4aa2579226b6831e7c41a9ea7d9229b807
sayankhan313 <khanshayan8828357863@gmail.com>
2025-06-27 22:28:04 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+
+// require('dotenv').config({path:'./env'});
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;(async ()=>{
+//     try {
+//        await  mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//        app.on('error',(error)=>{
+//               console.error("Error in app:", error);
+//               throw error;
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`Server is running on port ${process.env.PORT}`);
+//        })
+        
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// })
